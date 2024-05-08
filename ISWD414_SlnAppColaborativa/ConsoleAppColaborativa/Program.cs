9aa830425f071fad9ccf68bf342e81225ddb2ca7
JonathanA203 <jonathan.angulo@epn.edu.ec>
2024-05-08 12:54:30 -0500

    Jonathan Angulo

--- ISWD414_SlnAppColaborativa/ConsoleAppColaborativa/Program.cs
+++ ISWD414_SlnAppColaborativa/ConsoleAppColaborativa/Program.cs
@@ -4,14 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Jonathan Angulo");
             Console.WriteLine("Hello, World!");
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
             Console.WriteLine("Hola, Kevin Quishpe");
             Console.WriteLine("Hola, DylanGranizo");
             Console.WriteLine("Hola, Andreina Pallo")
 >>>>>>> 1984d480bb1e92cf5dbf859740fd4bffc9cd80ce
+=======
+            Console.WriteLine("Hola, Kevin Quishpe");
+            Console.WriteLine("Hola, DylanGranizo");
+            Console.WriteLine("Hola, Andreina Pallo")
+            Console.WriteLine("Hola, Erick Mideros")
+>>>>>>> 6b047d62a75b71e7ebdd97d4ad56fa14a5e37ffb
+
+            Console.WriteLine("Hello, Jonathan Angulo");
         }
     }
 }
