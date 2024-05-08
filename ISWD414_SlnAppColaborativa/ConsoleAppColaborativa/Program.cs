d66fefa34e38e6815cbc4b24749999f2b1f88c0a
JAIME SEBASTIAN SANCHEZ GORDON <jaime.sanchez02@epn.edu.ec>
2024-05-08 18:11:34 -0500

    commit de sebas

--- ISWD414_SlnAppColaborativa/ConsoleAppColaborativa/Program.cs
+++ ISWD414_SlnAppColaborativa/ConsoleAppColaborativa/Program.cs
@@ -14,6 +14,7 @@
             Console.WriteLine("Hello, Jonathan Angulo");
             Console.WriteLine("Hola, Oscar Tumbaco");
             Console.WriteLine("Hola, Paul Salas");
+            Console.WriteLine("Hola, Sebastián Sánchez");
         }
     }
 }
