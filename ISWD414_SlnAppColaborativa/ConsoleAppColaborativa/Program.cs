1728577d2b4a9f07b9803e3dd088b50f8dc8343f
JNaranjo12 <juan.naranjo01@epn.edu.ec>
2024-05-08 12:47:53 -0500

    Juan Naranjo

--- ISWD414_SlnAppColaborativa/ConsoleAppColaborativa/Program.cs
+++ ISWD414_SlnAppColaborativa/ConsoleAppColaborativa/Program.cs
@@ -5,13 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
             Console.WriteLine("Hi, Juan Naranjo");
-
-=======
             Console.WriteLine("Hola, Kevin Quishpe");
-
->>>>>>> 7327e9a946e0cacc9f71adf90f960f22cad0f651
         }
     }
 }
