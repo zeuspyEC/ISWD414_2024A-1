b5320b09a5c9c8d2fd563a4aaef2efdff0600bfd
JNaranjo12 <juan.naranjo01@epn.edu.ec>
2024-05-08 12:54:37 -0500

    Juan Naranjo

--- ISWD414_SlnAppColaborativa/ConsoleAppColaborativa/Program.cs
+++ ISWD414_SlnAppColaborativa/ConsoleAppColaborativa/Program.cs
@@ -1,11 +1,18 @@
-﻿namespace ConsoleAppColaborativa
+﻿using System.Globalization;
+
+namespace ConsoleAppColaborativa
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello, World!");
             Console.WriteLine("Hola, Kevin Quishpe");
             Console.WriteLine("Hola, DylanGranizo");
+            Console.WriteLine("Hola, Andreina Pallo")
+            Console.WriteLine("Hola, Erick Mideros")
             Console.WriteLine("Hi, Juan Naranjo");
+
         }
 }
